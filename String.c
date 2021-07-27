dbae20b0026db03c09595c4208fdbc3bc28c69c5
Ankita Prasad Gupta <64923045+ankitaapg17@users.noreply.github.com>
2021-07-27 22:58:13 +0530

    Add files via upload

---
+++ String.c
@@ -0,0 +1,24 @@
+//Basic string operations
+//Length of the string without using strlen() function
+//Toggle each character in a string
+//Count the number of vowels
+//Remove vowels from a string
+//String is a palindrome or not
+//Sorting a string in alphabetical order
+//Removing brackets from an algebraic expression
+//Remove characters in a string except alphabets
+//Remove spaces from a string
+//Count the sum of numbers in a string
+//Capitalize the first and last letter of each word of a string
+//Frequency of characters in a string
+//Non-repeating characters in a string
+//Check if two strings are Anagram or not
+//Patterns of 0(1+)0 in the given string
+//Replace substring in a string
+//Count common subsequence in two strings
+//Reverse a string
+//Check if two strings match where one string contains wildcard characters
+
+
+
+
