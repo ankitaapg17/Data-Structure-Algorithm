bab77d848ca33ce327c49bf010e721f52793bcb0
Ankita Prasad Gupta <64923045+ankitaapg17@users.noreply.github.com>
2021-07-28 10:22:27 +0530

    Add files via upload

---
+++ Arrays2.c
@@ -0,0 +1,23 @@
+//Maximum product subarray in a given array
+//Arrays are disjoint or not
+//Array is a subset of another array or not
+//Can all numbers of an array be made equal
+//Minimum sum of absolute difference of given array
+//Sorting elements of an array by frequency
+//Sort an array according to the order defined by another array
+//Replace each element of the array by its rank in the array
+//Equilibrium index of an array
+//Array rotation - Left and right
+//Block swap algorithm for array rotation
+//Juggling algorithm for array rotation
+//Circular rotation of an array by K positions
+//Convert an array into a zig-zag fashion
+//Merge two sorted arrays
+//Longest subarray having an average greater than or equal to k
+//Rearrange positive and negative numbers in an array
+//Sum of all odd frequency elements in an array
+//Median of two sorted arrays
+
+
+
+
