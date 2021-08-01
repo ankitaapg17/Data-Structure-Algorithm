a6e60a18c103f76df2ed274b8bd184d9f58b87c3
Ankita Prasad Gupta <64923045+ankitaapg17@users.noreply.github.com>
2021-08-01 21:18:50 +0530

    Add files via upload

---
+++ Hashing.cpp
@@ -0,0 +1,20 @@
+//Sorting elements of an array by frequency (hashmap).
+//Sort an array according to the order defined by another array
+//Replace each element of the array by its rank in the array
+
+
+
+
+
+
+//Using a hashing mechanism, we can store the elements (also first index) 
+//and their counts in a hash. Finally, sort the hash elements according to their counts.
+
+
+
+
+
+
+
+
+
